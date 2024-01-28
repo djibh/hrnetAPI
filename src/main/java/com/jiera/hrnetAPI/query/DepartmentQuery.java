fcde366651518ac8ab0c5694b27969caf5cb7c8c
Djibh <julien.riera@protonmail.com>
2024-01-28 16:22:05 +0100

    refactor(DEPARTMENT) > Update GetAll_Query for ASC sorting

--- src/main/java/com/jiera/hrnetAPI/query/DepartmentQuery.java
+++ src/main/java/com/jiera/hrnetAPI/query/DepartmentQuery.java
@@ -1,6 +1,6 @@
 package com.jiera.hrnetAPI.query;
 
 public class DepartmentQuery {
-    public static final String GET_ALL_DEPARTMENTS_QUERY = "SELECT * FROM Departments;";
+    public static final String GET_ALL_DEPARTMENTS_QUERY = "SELECT * FROM Departments ORDER BY title;";
     public static final String INSERT_DEPARTMENT_QUERY = "INSERT INTO Departments (title, manager, phone) VALUES (:title, :manager, :phone)";
 }
